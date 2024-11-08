6ac581e1586c93e105eab9090acda847ce466ff9
akatief <f.tiblias@live.it>
2024-11-08 13:10:05 +0100

    feat: add max_len parameter to qlstm

--- ham_classifier/baseline.py
+++ ham_classifier/baseline.py
@@ -262,11 +262,12 @@ class QLSTMClassifier(nn.Module, KWArgsMixin, UpdateMixin):
     Quantum LSTM classifier
     Heavily inspired by the QLSTM implementation in https://github.com/rdisipio/qlstm
     '''
-    def __init__(self, emb_dim, hidden_dim, n_wires, n_layers, gates, n_classes):
+    def __init__(self, emb_dim, hidden_dim, n_wires, n_layers, gates, n_classes, max_len=300):
         super(QLSTMClassifier, self).__init__()
         self.hidden_dim = hidden_dim
         self.n_classes = n_classes
-        
+        self.max_len = max_len
+
         # Single LSTM cell
         self.qlstm_cell = QLSTMCell(emb_dim, hidden_dim, n_wires, n_layers, gates)
         
@@ -285,6 +286,9 @@ class QLSTMClassifier(nn.Module, KWArgsMixin, UpdateMixin):
     def forward(self, x, seq_lengths):
         batch_size, seq_len, _ = x.size()
         
+        # Clamp sequence lengths to max_len
+        seq_lengths = torch.clamp(seq_lengths, max=self.max_len)
+
         # Initialize hidden state and cell state to zeros
         hidden = (torch.zeros(batch_size, self.hidden_dim).to(x.device),
                     torch.zeros(batch_size, self.hidden_dim).to(x.device))
@@ -298,12 +302,6 @@ class QLSTMClassifier(nn.Module, KWArgsMixin, UpdateMixin):
         
         # Concatenate outputs for all time steps
         outputs = torch.cat(outputs, dim=1)
-        
-        # # Use the output of the last time step for classification
-        # final_output = outputs[:, -1, :]  # Take the output of the last time step
-        
-        # # Pass the final output through the classifier
-        # normalized_output = self.classifier(final_output)
         
         # Use the last valid output for classification
         seq_lengths = seq_lengths.to(device=x.device)
